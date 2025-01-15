e0f985953ab5f7fecd7cc743678c731f090cc1f9
ClooverDev <130509634+ClooverDev@users.noreply.github.com>
2025-01-15 17:55:21 -0300

    Update updateProgressBar.js

--- updateProgressBar.js
+++ updateProgressBar.js
@@ -2,8 +2,8 @@ function update_progress_bar() {
 	const inner_bar = "#bar_inner";
 	const bar_title = document.getElementById("bar_title");
 	
-	const CURRENT_PROGRESS_INT = 100;
-	const CURRENT_PROGRESS_TITLE = "SUPER PLATFORM: R | HUNT OR HUNTED UPDATE";
+	const CURRENT_PROGRESS_INT = 0;
+	const CURRENT_PROGRESS_TITLE = "SUPER PLATFORM: R | PORTUGUESE TRANSLATION";
 	
 	$(inner_bar).width(CURRENT_PROGRESS_INT+"%");
 	bar_title.innerHTML = CURRENT_PROGRESS_TITLE+" <br><span style='color:white; font-size: 32px;'>["+CURRENT_PROGRESS_INT+"%]</span>";
